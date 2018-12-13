f4a87443960ee02d149bd7c9f56a47596c115574
nthhtn <thehien115@gmail.com>
2018-12-13 02:53:45 +0200

    Change env

--- config/mongo.js
+++ config/mongo.js
@@ -1,11 +1,11 @@
-module.exports = {
-	FI: 'mongodb://localhost/autoko-fi',
-	SE: 'mongodb://localhost/autoko-se',
-	NO: 'mongodb://localhost/autoko-no'
-};
-
 // module.exports = {
-// 	FI: 'mongodb://ec2-3-80-251-37.compute-1.amazonaws.com/autoko',
-// 	SE: 'mongodb://ec2-54-167-27-126.compute-1.amazonaws.com/autoko',
-// 	NO: 'mongodb://ec2-35-175-215-95.compute-1.amazonaws.com/autoko'
+// 	FI: 'mongodb://localhost/autoko-fi',
+// 	SE: 'mongodb://localhost/autoko-se',
+// 	NO: 'mongodb://localhost/autoko-no'
 // };
+
+module.exports = {
+	FI: 'mongodb://ec2-3-80-251-37.compute-1.amazonaws.com/autoko',
+	SE: 'mongodb://ec2-54-167-27-126.compute-1.amazonaws.com/autoko',
+	NO: 'mongodb://ec2-35-175-215-95.compute-1.amazonaws.com/autoko'
+};
